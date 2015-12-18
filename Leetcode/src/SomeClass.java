5bbcf217397466954505e0d477f8a1a5544819db
maryjavan <maryam.javanmardy@gmail.com>
2015-12-18 17:32:45 -0500

    Second

--- Leetcode/src/SomeClass.java
+++ Leetcode/src/SomeClass.java
@@ -3,37 +3,25 @@ public class SomeClass {
 	    
 	    public int compareVersion(String version1, String version2) {
 	    
-	    int cursor = 0;
+	    
 		String[] first = version1.split(".");
 		String[] second = version2.split(".");
 		int i=0;
 				
 		while (i < first.length && i < second.length ){
-			if (version1.charAt(cursor)== '.') {
-				first[i] = Integer.parseInt(version1.substring(j,cursor));
-				i++;
-				j=cursor+1;
-				}
-		cursor++;
+			if (Integer.parseInt(first[i]) < Integer.parseInt(second[i])) return -1;
+			else if (Integer.parseInt(first[i]) > Integer.parseInt(second[i])) return 1; 
+			i++;
 		}
 		
-		cursor = 0;
-		i=0;
-		j=0;
-		
-		while (cursor < version2.length()){
-			if (version2.charAt(cursor)== '.') {
-				second[i] = Integer.parseInt(version2.substring(j,cursor));
-				i++;
-				j=cursor+1;
-				}
-		cursor++;
+		while (first.length >= i) {
+			if (Integer.parseInt(first[i])!= 0) return 1;
+			i++;
 		}
 		
-		i=0;
-		while (i<5) {
-			if (first[i] < second[i]) return -1;
-			else if (first[i] > second[i]) return 1;
+		while (second.length >= i) {
+			if (Integer.parseInt(second[i])!= 0) return -1;
+			i++;
 		}
 		return 0;
 	    }
