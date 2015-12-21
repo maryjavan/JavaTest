aa3f68a9b61937bb539ed9a1ecd65216aa67f332
Saeid Mirzaei <smirzai@gmail.com>
2015-12-21 16:21:47 +0100

    restructure for tests, add some comments for LastWorld

--- Leetcode/src/main/LastWord.java
+++ Leetcode/src/main/LastWord.java
@@ -6,9 +6,12 @@ public class LastWord {
         while (s.length() > 0 && s.charAt(s.length()-1) == ' ') {
            s=s.substring(0,s.length()-1);
        }
+       // is it not better to use?  s = s.trim();
        
        int cursor = s.length()-1;
        
+       
+       // is the next line really necessary ? What happens if it is not there ?
        if (s.length() == 0 ) return 0;
        
        
