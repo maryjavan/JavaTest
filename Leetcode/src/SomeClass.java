f753ed02d5cddd841d743931e0574cc8b4f46066
maryjavan <maryam.javanmardy@gmail.com>
2015-12-21 11:20:50 -0500

    2

---
+++ Leetcode/src/SomeClass.java
@@ -0,0 +1,34 @@
+
+
+public class SomeClass {
+	    
+	    public static int compareVersion(String version1, String version2) {
+	    
+	    
+		String[] first = version1.split("//.");
+		String[] second = version2.split("//.");
+		int i=0;
+				
+		while (i < first.length && i < second.length ){
+			if (Integer.parseInt(first[i]) < Integer.parseInt(second[i])) return -1;
+			else if (Integer.parseInt(first[i]) > Integer.parseInt(second[i])) return 1; 
+			i++;
+		}
+		
+		while (first.length > i) {
+			if (Integer.parseInt(first[i])!= 0) return 1;
+			i++;
+		}
+		
+		while (second.length > i) {
+			if (Integer.parseInt(second[i])!= 0) return -1;
+			i++;
+		}
+		return 0;
+	    }
+	    
+	    public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	    	System.out.println(compareVersion("1.0","1.1"));
+		}	    
+	}
