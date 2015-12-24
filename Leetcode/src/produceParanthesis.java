8f0af650f1563cace4feb6e3c80b79abcac07a63
maryjavan <maryam.javanmardy@gmail.com>
2015-12-24 12:54:05 -0500

    2-3

---
+++ Leetcode/src/produceParanthesis.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+
+public class produceParanthesis {
+
+public static List<String> generateParenthesis(int n) {
+     
+	List<String> l = new ArrayList<String>();
+	List<String> temp = new ArrayList<String>();
+
+	if ( n == 2) l.add("()");
+	
+	else {
+		
+		temp = generateParenthesis(n-1);
+		Iterator<String> it= temp.iterator();
+		
+		while (it.hasNext()){
+			String s = it.next();
+			String left = "()"+ s;
+			String right = s + "()";
+			if (left.equals(right) ) l.add(right);
+			else {
+			l.add("()"+ s);
+			l.add(s + "()");
+			}
+			l.add("("+s+")");
+		}
+	
+	return l;
+		
+			
+	}
+	
+	return l;
+    }
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(generateParenthesis(4));
+	}
+
+}
