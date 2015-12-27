59c36a27227030c3ad2bbee86d50815254f0976f
Saeid Mirzaei <smirzai@gmail.com>
2015-12-27 16:19:43 +0100

    review Paranthesis

--- Leetcode/src/produceParanthesis.java
+++ Leetcode/src/produceParanthesis.java
@@ -4,76 +4,67 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
-
-
 public class produceParanthesis {
 
-public static List<String> generateParenthesis(int n) {
-     
-	List<String> l = new ArrayList<String>();
-	List<String> temp = new ArrayList<String>();
-	Set<String> hs = new HashSet<String>();
+	public static List<String> generateParenthesis(int n) {
 
-	if (n<1) return null;
-	
-	for (int i=1; i <= n; i++){
-		if ( i == 1) l.add("()");
-		
-		else {
-			temp.clear();
-			temp.addAll(l);
-			Iterator<String> it= temp.iterator();
-			l.clear();
-			
-			while (it.hasNext()){
-				String s = it.next();
-				l.add("("+s+")");
-				String left = "()"+ s;
-				String right = s + "()";
-				if (left.equals(right) ) l.add(right);
-				else {
-				l.add(left);
-				l.add(right);
-				}
-			}
-						
-		}
-    }
+		List<String> l = new ArrayList<String>();
+		List<String> temp = new ArrayList<String>();
+		Set<String> hs = new HashSet<String>();
 
-	
-	hs.addAll(l);
-	l.clear();
-	l.addAll(hs);	
-	return l;	
-	}
-	/*
-	
-	if ( n == 1) l.add("()");
-	
-	else {
-		
-		temp = generateParenthesis(n-1);
-		Iterator<String> it= temp.iterator();
-		
-		while (it.hasNext()){
-			String s = it.next();
-			l.add("("+s+")");
-			String left = "()"+ s;
-			String right = s + "()";
-			if (left.equals(right) ) l.add(right);
+		if (n < 1)
+			return null;
+
+		for (int i = 1; i <= n; i++) {
+			if (i == 1)
+				l.add("()");
+
 			else {
-			l.add(left);
-			l.add(right);
+				temp.clear();
+				temp.addAll(l);
+				Iterator<String> it = temp.iterator();
+				l.clear();
+
+				while (it.hasNext()) {
+					String s = it.next();
+					l.add("(" + s + ")");
+					String left = "()" + s;
+					String right = s + "()";
+					if (left.equals(right))
+						l.add(right);
+					else {
+						l.add(left);
+						l.add(right);
+					}
+				}
+
 			}
 		}
-	
-	return l;
-		
-			
+
+		hs.addAll(l);
+		l.clear();
+		l.addAll(hs);
+		return l;
 	}
-	*/
-	
-	
+
+	/*
+	 * 
+	 * if ( n == 1) l.add("()");
+	 * 
+	 * else {
+	 * 
+	 * temp = generateParenthesis(n-1); Iterator<String> it= temp.iterator();
+	 * 
+	 * while (it.hasNext()){ String s = it.next(); l.add("("+s+")"); String left
+	 * = "()"+ s; String right = s + "()"; if (left.equals(right) )
+	 * l.add(right); else { l.add(left); l.add(right); } }
+	 * 
+	 * return l;
+	 * 
+	 * 
+	 * }
+	 */
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(generateParenthesis(4));
